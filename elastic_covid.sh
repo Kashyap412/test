d14791de319833f5ed4244265f6e4b600e3be478
Kashyap <43431636+Kashyap412@users.noreply.github.com>
2023-03-24 12:11:18 +0530

    Delete elastic_covid.sh

--- elastic_covid.sh
+++
@@ -1,4 +0,0 @@
-curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.4.3-linux-x86_64.tar.gz
-tar xzvf elastic-agent-8.4.3-linux-x86_64.tar.gz
-cd elastic-agent-8.4.3-linux-x86_64
-sudo ./elastic-agent install --url=https://45.118.183.209:8220 --enrollment-token=T2U5aWtJSUI3a0UxMjZsV09yYVQ6TXFSNDRLTW9UYzZ3LWN1NmxFNjAzQQ==
